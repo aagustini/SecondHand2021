d7f8513ffa98d460d4553562ec714c138cc474f1
AAgustini <alexandre.agustini@pucrs.br>
2021-04-12 19:47:21 -0300

    correcao na classe de teste

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleTests
             _adm.Add(new Grupo()
             {
                 Nome = "Grupo campeao",
-                Integrantes = "<alu1, nome1> , <alu2,nome2>",
+                Integrantes = "<matr1, nome1> , <matr2,nome2>,...",
                 GitHub = "http://github.com/meuprojetoPrivado",
                 Comentarios = "Teste de insercao"
             });
@@ -22,14 +22,14 @@ namespace ConsoleTests
             _adm.Add(new Grupo()
             {
                 Nome = "Grupo dois",
-                Integrantes = "<alu2.1, nome2.1> , <alu2.2,nome2.2>",
+                Integrantes = "<matr2.1, nome2.1> , <matr2.2,nome2.2>",
                 GitHub = "http://github.com/projeto2Privado",
                 Comentarios = "So para teste nao faz sentido dois grupos"
             });
 
             foreach (Grupo g in _adm.GetGrupos())
             {
-                Console.WriteLine("Nome: {0}\n\tid: {1}\n\tComponentes: {2}\n\tComponentes: {3}\n",
+                Console.WriteLine("Nome: {0}\n\tid: {1}\n\tComponentes: {2}\n\tComentarios: {3}\n",
                     g.Nome, g.GrupoId, g.Integrantes, g.Comentarios);
 
             }
